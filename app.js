b3ac5c26aa8724bb311f5bc5179cd6129ef09766
ozubergs <34900230+ozubergs@users.noreply.github.com>
2019-07-20 00:43:14 -0400

    Initail Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require("ejs")
+
+const app = express();
+
+app.set("view engine", "ejs")
+
+//renders public files(css or images)
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render("home")
+});
+
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000.");
+});
