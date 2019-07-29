0818e3885d9e57788161507c1d3c55b99ace48b8
ozubergs <34900230+ozubergs@users.noreply.github.com>
2019-07-29 02:28:41 -0400

    Add new section, top Sign Up form and response

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const ejs = require("ejs")
+const ejs = require("ejs");
+
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.render("home");
+});
+
+app.get('/signup', function(req, res) {
+  res.render("signup");
+});
+
+app.post('/signup', function(req, res) {
+  console.log("did not installed body request yet")
+});
+
+app.post('/login', function(req,res) {
+  console.log("did not installed body request yet")
 });
 
 app.listen(3000, function() {
